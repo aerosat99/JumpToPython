d6013d31371e8103ca48d9c83041862113feac10
aerosat99 <40327273+aerosat99@users.noreply.github.com>
2018-06-17 10:02:04 +0900

    Add files via upload

---
+++ 01-2. simple.py
@@ -0,0 +1,14 @@
+languages = ['python', 'perl', 'c', 'java']
+
+for lang in languages :
+    if lang in ['python', 'perl' ] :
+        print("%6s need interpreter" % lang)
+    elif lang in ['c', 'java'] :
+        print("%6s need complier" % lang)
+    else :
+        print("should not reach here")
+'''
+주석이 여러줄일때
+name = ""
+tel = ""
+'''
